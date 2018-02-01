b8794f0a26eec3ec0f5a68487c4dd12b0b51e321
Tiffany <tiffanyc.lehigh@gmail.com>
2018-01-31 20:57:05 -0500

    Added audio to notify timer end. Changed timer to show sessions over 60 minutes.
    Audio file was added to HTML and will be played once the time on timer reaches 0.  The audio is stopped when user clicks anywhere on the screen.
    
    Timer used to only display minutes and seconds. Added time conversion for day and hour. Timer now will display the hour if the user chooses a session time over 60 minutes.

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
 A Pen created at CodePen.io. You can find this one at https://codepen.io/tiffanymakes/pen/ZaqROL.
 
- Under Construction
+ A pomodoro timer that can be customized to desired work session length.
--- js/index.js
+++ js/index.js
@@ -1,47 +1,54 @@
 // Display session time
 let session = document.getElementById('session').value;
-//let breakt = document.getElementById('break').value;
-document.getElementById('display').innerHTML = session;
+document.getElementById('display').innerHTML = `${session} minutes`;
 
-// Store and display session time change
-document.getElementById('session').addEventListener('input', sessionTime);
-function sessionTime() {
+ // Store and display session time change
+document.getElementById('session').addEventListener('input', function () {
+  clearInterval(timerInterval);
   session = document.getElementById('session').value;
-  document.getElementById('display').innerHTML = session;
-}
+  document.getElementById('display').innerHTML = `${session} minutes`;
+});
 
-// Store break time change
-/*
-document.getElementById('break').addEventListener('input', breakTime);
-function breakTime() {
-  breakt = document.getElementById('session').value;
-}
-*/
+// Reset timer
+document.getElementById('reset').addEventListener('click', function() {
+    clearInterval(timerInterval);
+    document.getElementById('display').innerHTML = `${session} minutes`;
+});
 
 // Run Pomodoro on click
 let timerInterval;
 document.getElementById('display').addEventListener('click', function() {
   clearInterval(timerInterval);
+
   // Countdown timer
   const start = Date.parse(new Date());
   const end = session*1000*60+start;
   timerInterval = setInterval(() => {
     const now = Date.parse(new Date());
     const t = end - now;
+    const d = Math.floor(t / (1000 * 60 * 60 * 24));
+    const h = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const m = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
     const s = Math.floor((t % (1000 * 60)) / 1000);
-    m < 10 && s < 10 ? document.getElementById('display').innerHTML = `0${m}:0${s}` 
+
+    console.log(d + ":" + h + ":" + m);
+
+    d > 0 ? document.getElementById('display').innerHTML = `Reduce session length`
+      : h > 0 && m < 10 && s < 10 ? document.getElementById('display').innerHTML = `${h}:0${m}:0${s}`
+      : h > 0 && m < 10 ? document.getElementById('display').innerHTML = `${h}:0${m}:${s}`
+      : h > 0 && s < 10 ? document.getElementById('display').innerHTML = `${h}:${m}:0${s}`
+      : h > 0 ? document.getElementById('display').innerHTML = `${h}:${m}:${s}`
+      : m < 10 && s < 10 ? document.getElementById('display').innerHTML = `0${m}:0${s}` 
       : m < 10 ? document.getElementById('display').innerHTML = `0${m}:${s}` 
       : s < 10 ? document.getElementById('display').innerHTML = `${m}:0${s}` 
       : document.getElementById('display').innerHTML = `${m}:${s}`;
 
     if (t <= 0) {
       clearInterval(timerInterval);
+      document.getElementById('play').play();
+      document.addEventListener('click', function () {
+        document.getElementById('play').pause()
+        });
     }
 }, 1000);
-});
-
-document.getElementById('reset').addEventListener('click', function() {
-    clearInterval(timerInterval);
-    document.getElementById('display').innerHTML = session;
 });
